0eba629d47288876fafa8211b84d6d42e0c1e5cb
Cristian Lucchesi <cristian.lucchesi@iit.cnr.it>
2023-02-09 15:03:59 +0100

    Configurazione security su openapi ed esempio su StampingsFromClient.

--- src/main/java/it/cnr/iit/epas/controller/StampingsFromClient.java
+++ src/main/java/it/cnr/iit/epas/controller/StampingsFromClient.java
@@ -20,6 +20,8 @@ package it.cnr.iit.epas.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import it.cnr.iit.epas.config.OpenApiConfiguration;
 import it.cnr.iit.epas.dto.v4.StampingDto;
 import it.cnr.iit.epas.dto.v4.StampingFromClientDto;
 import it.cnr.iit.epas.dto.v4.mapper.StampingDtoMapper;
@@ -32,6 +34,7 @@ import lombok.val;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,6 +55,7 @@ class StampingsFromClient {
 
   @Operation(
       summary = "Inserisce una timbratura ricevuta nel formato utilizzato dai client di ePAS")
+  @SecurityRequirement(name = OpenApiConfiguration.BASIC_AUTHENTICATION)
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "timbratura inserita correttamente"),
       @ApiResponse(responseCode = "400", description = "dati timbratura non corretti oppure"
@@ -65,22 +69,35 @@ class StampingsFromClient {
       @ApiResponse(responseCode = "409", description = "timbratura già presente")
   })
   @PutMapping("/create")
-  ResponseEntity<StampingDto> create(@NotNull StampingFromClientDto stampingFromClientDto) {
+  ResponseEntity<StampingDto> create(
+      @NotNull @RequestBody StampingFromClientDto stampingFromClientDto) {
+
+    log.debug("Ricevuta richiesta creazione timbratura -> {}", stampingFromClientDto);
+
+    if (stampingFromClientDto == null) {
+      log.info("Ricevuta richiesta di creazione timbratura senza informazioni json nel body");
+      return ResponseEntity.badRequest().build();
+    }
 
     val stampingFromClient = stampingFromClientDto.convert();
+    if (stampingFromClient.isEmpty()) {
+      log.info("Ricevuta richiesta di creazione timbratura con informazioni incomplete"
+          + " o errate nel body");
+      return ResponseEntity.badRequest().build();
+    }
 
     // Badge number not present (404)
-    if (!stampingManager.linkToPerson(stampingFromClient).isPresent()) {
+    if (!stampingManager.linkToPerson(stampingFromClient.get()).isPresent()) {
       return ResponseEntity.notFound().build();
     }
 
     // Controllo timbratura con data troppo vecchia
-    if (stampingManager.isTooFarInPast(stampingFromClient.getDateTime())) {
+    if (stampingManager.isTooFarInPast(stampingFromClient.get().getDateTime())) {
       log.info("Ignorata timbratura con data troppo nel passato: {}", stampingFromClient);
       return ResponseEntity.badRequest().build();
     }
 
-    val stamping = stampingManager.createStampingFromClient(stampingFromClient, true);
+    val stamping = stampingManager.createStampingFromClient(stampingFromClient.get(), true);
 
     // Stamping already present (409)
     if (!stamping.isPresent()) {
