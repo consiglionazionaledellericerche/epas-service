eef54809a6ac7942863042f3e2b190c03484c150
Andrea Generosi <and_gen@hotmail.com>
2024-10-09 12:05:06 +0200

    implementazione endpoint per recuperare le ore di formazione

--- src/main/java/it/cnr/iit/epas/controller/v4/PersonMonthsController.java
+++ src/main/java/it/cnr/iit/epas/controller/v4/PersonMonthsController.java
@@ -28,27 +28,29 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import it.cnr.iit.epas.config.OpenApiConfiguration;
 import it.cnr.iit.epas.controller.v4.utils.ApiRoutes;
-import it.cnr.iit.epas.controller.v4.utils.PersonFinder;
+import it.cnr.iit.epas.dao.PersonMonthRecapDao;
 import it.cnr.iit.epas.dao.wrapper.IWrapperContract;
 import it.cnr.iit.epas.dao.wrapper.IWrapperContractMonthRecap;
 import it.cnr.iit.epas.dao.wrapper.IWrapperFactory;
-import it.cnr.iit.epas.dto.v4.ContractMonthRecapDto;
+import it.cnr.iit.epas.dto.v4.PersonMonthRecapDto;
 import it.cnr.iit.epas.dto.v4.PersonMonthsDto;
+import it.cnr.iit.epas.dto.v4.TrainingHoursDto;
+import it.cnr.iit.epas.dto.v4.mapper.PersonMonthRecapMapper;
 import it.cnr.iit.epas.dto.v4.mapper.PersonMonthsMapper;
-import it.cnr.iit.epas.dto.v4.mapper.PersonStampingRecapMapper;
-import it.cnr.iit.epas.manager.recaps.personstamping.PersonStampingRecapFactory;
+import it.cnr.iit.epas.dto.v4.mapper.TrainingHoursMapper;
 import it.cnr.iit.epas.models.Contract;
 import it.cnr.iit.epas.models.ContractMonthRecap;
 import it.cnr.iit.epas.models.Person;
+import it.cnr.iit.epas.models.PersonMonthRecap;
 import it.cnr.iit.epas.models.User;
 import it.cnr.iit.epas.security.SecureUtils;
 import it.cnr.iit.epas.security.SecurityRules;
 import java.time.YearMonth;
-import lombok.val;
-import org.joda.time.LocalDate;
-//import org.joda.time.YearMonth;
+import java.util.ArrayList;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import javax.transaction.Transactional;
 import javax.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -63,7 +65,6 @@ import org.springframework.web.bind.annotation.RestController;
  * Controller con i metodi REST relativi alla gestione dei PersonMonths.
  *
  * @author Cristian Lucchesi
- *
  */
 @SecurityRequirements(
     value = {
@@ -72,6 +73,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(
     name = "PersonMonths controller",
     description = "Visualizzazione dei riepiloghi orari e formazione dei dipendenti.")
+@Transactional
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -80,6 +82,9 @@ public class PersonMonthsController {
 
   private final IWrapperFactory wrapperFactory;
   private final PersonMonthsMapper personMonthsMapper;
+  private final PersonMonthRecapMapper personMonthRecapMapper;
+  private final TrainingHoursMapper trainingHoursMapper;
+  private final PersonMonthRecapDao personMonthRecapDao;
   private final SecurityRules rules;
   private final SecureUtils securityUtils;
 
@@ -116,7 +121,7 @@ public class PersonMonthsController {
       return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
     }
 
-    if (year > new LocalDate().getYear()) {
+    if (year > LocalDate.now().getYear()) {
       //flash.error("Impossibile richiedere riepilogo anno futuro.");
       //renderTemplate("Application/indexAdmin.html");
       return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
@@ -130,7 +135,7 @@ public class PersonMonthsController {
     List<IWrapperContractMonthRecap> recaps = Lists.newArrayList();
     IWrapperContractMonthRecap wp;
     YearMonth actual = YearMonth.of(year, 1);
-    YearMonth last =  YearMonth.of(year, 12);
+    YearMonth last = YearMonth.of(year, 12);
     IWrapperContract con = wrapperFactory.create(contract.get());
     while (!actual.isAfter(last)) {
       Optional<ContractMonthRecap> recap = con.getContractMonthRecap(actual);
@@ -142,5 +147,48 @@ public class PersonMonthsController {
       actual = actual.plusMonths(1);
     }
     return ResponseEntity.ok().body(pmListDto);
+  }
+
+  @Operation(
+      summary = "Visualizzazione del riepilogo delle ore di formazione annuale del dipendente.",
+      description = "Questo endpoint è utilizzabile dalle persone autenticate per visualizzare "
+          + "la propria situazione delle ore di formazione, oppure dagli utenti con il ruolo "
+          + "'Amministratore del personale' della sede a cui appartiene la persona, oppure dagli "
+          + "utenti con il ruolo di sistema 'Developer' e/o 'Admin'.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200",
+          description = "Restituiti i dati dei riepiloghi delle ore di formazione"),
+      @ApiResponse(responseCode = "401",
+          description = "Autenticazione non presente", content = @Content),
+      @ApiResponse(responseCode = "403",
+          description = "Utente che ha effettuato la richiesta non autorizzato a visualizzare"
+              + " i dati del riepilogo delle ore di formazione.",
+          content = @Content),
+      @ApiResponse(responseCode = "404",
+          description = "Riepilogo ore di formazione non trovato con l'anno fornito",
+          content = @Content)
+  })
+  @GetMapping("/trainingHours")
+  ResponseEntity<TrainingHoursDto> trainingHours(
+      @NotNull @RequestParam("year") Integer year) {
+    log.debug("REST method {} invoked with parameters year={}",
+        "/rest/v4/personmonths/trainingHours", year);
+
+    Person person = securityUtils.getCurrentUser().get().getPerson();
+
+    List<PersonMonthRecap> personMonthRecapList = personMonthRecapDao
+        .getPersonMonthRecapInYearOrWithMoreDetails(person, year,
+            Optional.empty(), Optional.empty());
+
+    LocalDate today = LocalDate.now();
+    List<PersonMonthRecapDto> dtoList = new ArrayList<>();
+    personMonthRecapList.forEach(pmr -> dtoList.add(personMonthRecapMapper.convert(pmr)));
+
+    TrainingHoursDto dto = new TrainingHoursDto();
+    dto.setPerson(trainingHoursMapper.convert(person));
+    dto.setToday(today);
+    dto.setPersonMonthRecaps(dtoList);
+
+    return ResponseEntity.ok().body(dto);
   }
 }
