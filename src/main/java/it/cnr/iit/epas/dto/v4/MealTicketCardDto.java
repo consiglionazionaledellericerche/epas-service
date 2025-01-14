7905f6be1bbc4f1b7954340b4f0b29661e4f7c28
Andrea Generosi <and_gen@hotmail.com>
2025-01-14 19:29:19 +0100

    sviluppo endpoint per reperire le informazioni dei buoni pasti di un dipendente

---
+++ src/main/java/it/cnr/iit/epas/dto/v4/MealTicketCardDto.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2023  Consiglio Nazionale delle Ricerche
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package it.cnr.iit.epas.dto.v4;
+
+import com.google.common.collect.Lists;
+import it.cnr.iit.epas.models.MealTicket;
+import it.cnr.iit.epas.models.Office;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.Data;
+
+/**
+ * DTO per la situazione ferie di una persona.
+ *
+ * @author Cristian Lucchesi
+ *
+ */
+@Data
+public class MealTicketCardDto {
+  private String number;
+
+  private PersonBaseDto person;
+
+  //List<MealTicketDto> mealTickets = Lists.newArrayList();
+  private boolean isActive;
+  private LocalDate deliveryDate;
+  private OfficeShowTerseDto deliveryOffice;
+}
